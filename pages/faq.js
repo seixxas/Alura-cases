015e12f82fb9eeb937afb673e88fc85e958f3665
Guilherme Seixas <guilhermeseixasabreu@gmail.com>
2022-06-02 02:50:23 -0300

    curso 1

---
+++ pages/faq.js
@@ -0,0 +1,56 @@
+
+import Head from 'next/head';
+import Link from '../src/components/Link';
+//SSG - Static Side Generation
+//SSR - Server SIde Rendering
+//ISG - Incremental Static Generation
+
+// export async function getServerSideProps(){
+// console.log('Em modo DEV, sempre roda! a cada acesso')    
+// console.log('Rodando a cada acesso que voce recebe')    
+
+
+export async function getStaticProps() {
+    console.log('Em modo DEV, sempre roda! a cada acesso')
+    console.log("isso roda em build time")
+    const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
+    const faq = await fetch(FAQ_API_URL)
+        .then((respostaDoServidor) => {
+            return respostaDoServidor.json();
+        })
+        .then((resposta) => {
+            return resposta;
+        });
+
+    return {
+        props: {
+            qualquercoisa: 'que eu passar aqui',
+            faq,
+        },
+    };
+}
+
+export default function FAQPage({ faq }) {
+    console.log('isso roda no servidor?')
+    return (
+        <div>
+            <Head>
+                <title>FAQ - Alura cases</title>
+            </Head>
+            <h1>Alura Cases - Páginas de Perguntas FAQ</h1>
+            <Link href="/">
+                Ir para a home
+            </Link>
+            <ul>
+                {faq.map(({ answer, question }) => (
+                    <li key={question}>
+                        <article>
+                            <h2>{question}</h2>
+                            <p>{answer}</p>
+                        </article>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
